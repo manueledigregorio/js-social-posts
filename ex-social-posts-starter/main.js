e26aa272ea72c292f6248e595049f8089d1933b4
manueledigregorio <dig.manuele@gmail.com>
2023-09-25 17:04:48 +0200

    [add] post

--- ex-social-posts-starter/main.js
+++ ex-social-posts-starter/main.js
@@ -55,3 +55,56 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+const container = document.querySelector('#container')
+posts.forEach(element => {
+   
+    container.innerHTML += 
+   `<div class="post">
+   <div class="post__header">
+       <div class="post-meta">                    
+           <div class="post-meta__icon">
+               <img class="profile-pic" src="${element.author.image}" alt="${element.name}">                    
+           </div>
+           <div class="post-meta__data">
+               <div class="post-meta__author">${element.name}</div>
+               <div class="post-meta__time">${element.created}</div>
+           </div>                    
+       </div>
+   </div>
+   <div class="post__text">${element.content}</div>
+   <div class="post__image">
+       <img src="${element.media}" alt="">
+   </div>
+   <div class="post__footer">
+       <div class="likes js-likes">
+           <div class="likes__cta">
+               <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
+                   <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                   <span class="like-button__label">Mi Piace</span>
+               </a>
+           </div>
+           <div class="likes__counter">
+               Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+           </div>
+       </div> 
+   </div>            
+</div>
+
+</div>
+` 
+});
+
+const btn = document.querySelectorAll('#like-counter-1')
+
+btn.addEventListener("click", function() {
+
+   incrementa(counter)
+
+    console.log(incrementa)
+})
+let counter = 0
+
+function incrementa(x) {
+    x=x+1;
+    document.write(x);
+}
